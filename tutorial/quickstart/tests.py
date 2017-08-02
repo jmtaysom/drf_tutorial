494868cebb4101f61ec356163376d46a792596fe
jmtaysom <james.taysom@gmail.com>
2017-08-02 19:29:15 -0400

    starting to work on tests

--- tutorial/quickstart/tests.py
+++ tutorial/quickstart/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIRequestFactory
 
-# Create your tests here.
+
+# Using the standard RequestFactory API to create a form POST request
+
+class AccountTests(APITestCase):
+    def test_create_user(self):
+        factory = APIRequestFactory()
+        request = factory.post('/users/', {'username': 'a person', 'email': 'some@address.com'})
+        response = self.client.get('/users/1/')
+        #self.assertEqual(request.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, {'username': 'a person', 'email': 'some@address.com'})
